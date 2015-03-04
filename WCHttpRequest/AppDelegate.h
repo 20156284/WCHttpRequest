0e2d38a4b01e9fb6d71963e55732d1a24dac2098
WillCheung <willzh@live.cn>
2015-03-04 11:30:06 +0800

    Initial Commit

---
+++ WCHttpRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCHttpRequest
+//
+//  Created by Will on 4/3/2015.
+//  Copyright (c) 2015年 Will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WCHttpRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WCHttpRequest
+//
+//  Created by Will on 4/3/2015.
+//  Copyright (c) 2015年 Will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
